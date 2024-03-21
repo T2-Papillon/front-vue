7917e1d178d717bef7924b391708175b344ce6bd
raheego <54056684+raheego@users.noreply.github.com>
2024-03-21 21:38:07 +0900

    pinia

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': 'http://localhost:8080',
+            rewrite: (path) => path.replace(/^\/api/, ''),
             changeOrigin: true
         }
     }
