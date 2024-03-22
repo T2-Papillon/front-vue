35887e25912605ade2c54173d298a5618cafff2a
raheego <raae517@gmail.com>
2024-03-22 12:28:05 +0900

    [add] chart

---
+++ src/components/common/PieChart.vue
@@ -0,0 +1,36 @@
+<template>
+    <Pie :data="data" :options="options" />
+</template>
+
+<script lang="ts">
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'vue-chartjs'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default {
+    name: 'App',
+    components: {
+        Pie
+    },
+    data() {
+        return {
+            chartData: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [
+                    {
+                        label: 'My First Dataset',
+                        data: [40, 20, 12, 14, 50, 45],
+                        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
+                        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+                        borderWidth: 1
+                    }
+                ]
+            },
+            chartOptions: {
+                responsive: true
+            }
+        }
+    }
+}
+</script>
