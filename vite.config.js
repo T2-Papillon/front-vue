79c40ddddad6b276bc377cc964b99266b1d5b555
raheego <raae517@gmail.com>
2024-03-22 17:34:31 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '/api': 'http://localhost:8080',
+            '/api': 'http://localhost:8081',
             changeOrigin: true
         }
     }
