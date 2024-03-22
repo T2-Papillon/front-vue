4f3e9803752fede2c5b719e4a463385d0e0b12cf
raheego <raae517@gmail.com>
2024-03-22 14:13:59 +0900

    header 내 nav 이관

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 
 import Header from './components/common/layout/HeaderApp.vue'
 import Footer from './components/common/layout/FooterApp.vue'
@@ -7,13 +7,7 @@ import Footer from './components/common/layout/FooterApp.vue'
 
 <template>
     <Header />
-    <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/login">로그인</RouterLink>
-        <RouterLink to="/kit">UI Kit</RouterLink>
-        <RouterLink to="/project">프로젝트</RouterLink>
-    </nav>
+
     <RouterView />
     <Footer />
 </template>
