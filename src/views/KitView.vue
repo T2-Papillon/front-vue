d015785e8149ffdd307d6715c712267c08f8d54e
raheego <raae517@gmail.com>
2024-03-26 10:23:40 +0900

    update

--- src/views/KitView.vue
+++ src/views/KitView.vue
@@ -198,12 +198,32 @@ export default {
                 <div class="card">
                     <div class="card-body">
                         <h3 class="h4">알림</h3>
-                        <ul class="mt-4">
-                            <li>
-                                <a href="#" class="h5">제목제목제목</a>
-                                <span class="desc">24.03.21</span>
-                            </li>
-                        </ul>
+                        <div class="list-group">
+                            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">List group item heading</h5>
+                                    <small>3 days ago</small>
+                                </div>
+                                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                                <small>And some small print.</small>
+                            </a>
+                            <a href="#" class="list-group-item list-group-item-action">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">List group item heading</h5>
+                                    <small class="text-body-secondary">3 days ago</small>
+                                </div>
+                                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                                <small class="text-body-secondary">And some muted small print.</small>
+                            </a>
+                            <a href="#" class="list-group-item list-group-item-action">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">List group item heading</h5>
+                                    <small class="text-body-secondary">3 days ago</small>
+                                </div>
+                                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                                <small class="text-body-secondary">And some muted small print.</small>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
