8705ab3307daa2124842b375b06eb1eb83e50747
raheego <54056684+raheego@users.noreply.github.com>
2024-03-27 11:48:51 +0900

    vue 환경설정

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,17 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
+    },
+    build: {
+        outDir: '../Spring/papplan/src/main/resources/static'
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
     }
 })
