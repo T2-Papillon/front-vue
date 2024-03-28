88396c0b83cb5e432c3d0765272da49d1561018d
raheego <54056684+raheego@users.noreply.github.com>
2024-03-28 19:05:35 +0900

    [ADD] Task 컴포넌트 분리 TasksTable.vue

--- src/views/ProjectDetailView.vue
+++ src/views/ProjectDetailView.vue
@@ -1,12 +1,14 @@
 <script>
 import { ref } from 'vue'
 import CheckboxSelector from '../components/CheckboxSelector.vue'
-import ProjectInfo from '../components/ProjectInfo.vue'
+// import ProjectInfo from '../components/ProjectInfo.vue'
+import TasksTable from '../components/TasksTable.vue'
 
 export default {
     components: {
         CheckboxSelector,
-        ProjectInfo
+        // ProjectInfo,
+        TasksTable
     },
     props: {
         project: Object // props로 project를 받아옴
@@ -55,7 +57,7 @@ export default {
             </div>
         </div>
 
-        <ProjectInfo :project="project" />
+        <!-- <ProjectInfo :project="project" /> -->
 
         <!-- 정렬 -->
         <div class="row align-items-start justify-content-between mb-4 g-3 border-top">
@@ -87,50 +89,11 @@ export default {
         </div>
 
         <!-- 하위업무 -->
-        <!-- <div class="row">
+        <div class="row">
             <div class="col">
-                <table class="table fs-9 mb-5 border-top border-translucent">
-                    <colgroup>
-                        <col />
-                        <col style="width: 80px" />
-                        <col style="width: 126px" />
-                        <col style="width: 126px" />
-                        <col style="width: 100px" />
-                        <col style="width: 180px" />
-                        <col style="width: 80px" />
-                        <col style="width: 126px" />
-                    </colgroup>
-                    <thead>
-                        <tr>
-                            <th class="sort white-space-nowrap align-middle" scope="col" data-sort="project_title">프로젝트명</th>
-                            <th class="sort align-middle" scope="col" data-sort="">담당자</th>
-                            <th class="sort align-middle" scope="col" data-sort="start_date">시작일</th>
-                            <th class="sort align-middle" scope="col" data-sort="end_date">종료일</th>
-                            <th class="sort text-start ps-5 align-middle" scope="col" data-sort="status">진행상태</th>
-                            <th class="sort text-end align-middle" scope="col" data-sort="progress">진행률</th>
-                            <th class="sort text-end align-middle" scope="col" data-sort="priority">우선순위</th>
-                            <th class="sort text-end pe-0 align-middle" scope="write_date">작성일</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(task, index) in tasks" :key="index">
-                            <td>
-                                <a href="#" class="tb-project-title">{{ task.title }}</a>
-                            </td>
-                            <td class="text-start">
-                                <UserProfile v-for="participant in task.participants" :key="participant" :name="participant" />
-                            </td>
-                            <td>{{ task.start_date }}</td>
-                            <td>{{ task.end_date }}</td>
-                            <td><StatusBadge :status="task.status" /></td>
-                            <td><ProgressBar :progress="task.progress" /></td>
-                            <td class="text-end"><PriorityBadge :priority="task.priority" /></td>
-                            <td class="text-end">{{ task.write_date }}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <TasksTable />
             </div>
-        </div> -->
+        </div>
     </div>
 </template>
 
