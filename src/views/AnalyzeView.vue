29cabf4114e827222c0199c5ea3ec1464cfff3c2
afresh2 <afresh320@daum.net>
2024-03-29 09:44:26 +0900

    [ADD]ProjectDetailView추가

--- src/views/AnalyzeView.vue
+++ src/views/AnalyzeView.vue
@@ -1,4 +1,6 @@
 <script>
+import { ref, onMounted } from 'vue'
+import ProjectInfo from '../components/ProjectInfo.vue'
 import PieChart from '../components/PieChart.vue'
 import TaskTable from '../components/TaskTable.vue'
 import ProjectTable from '../components/ProjectTable.vue'
@@ -6,18 +8,57 @@ import ProjectThisWeek from '../components/ProjectThisWeek.vue'
 
 export default {
     components: {
+        ProjectInfo,
         PieChart,
         TaskTable,
         ProjectTable,
         ProjectThisWeek
     },
     data() {
-        return {}
+        return {
+            projects: []
+        }
+    },
+    setup() {
+        const project = ref({})
+        const checkboxItems = ref([
+            { id: 'todo', name: '진행예정' },
+            { id: 'doing', name: '진행중' },
+            { id: 'done', name: '완료' },
+            { id: 'hold', name: '보류' }
+        ])
+
+        async function fetchProjectDetail() {
+            try {
+                const apiUrl = import.meta.env.VITE_API_URL
+                const response = await axios.get(`${apiUrl}/search`)
+                console.log(response.data)
+
+                project.value = response.data
+            } catch (error) {
+                console.error('프로젝트 데이터를 가져오는데 실패했습니다:', error)
+            }
+        }
+
+        onMounted(() => {
+            fetchProjectDetail()
+        })
+
+        return {
+            project,
+            checkboxItems
+        }
+    },
+
+    mounted() {
+        this.fetchProjectDetail()
     }
 }
 </script>
 <template>
     <div class="inner">
+        <!-- 프로젝트 정보 -->
+        <ProjectInfo :project="project" />
         <div class="row align-items-start justify-content-between g-3">
             <div class="col-auto">
                 <div class="title-area">
