c75e5a15acb1e12fa2d7a3a0e2d8c5603393be92
dana096 <lovejiwon11@naver.com>
2024-04-01 17:49:20 +0900

    [ADD] ui 정돈 진행

--- src/components/layout/HeaderApp.vue
+++ src/components/layout/HeaderApp.vue
@@ -1,21 +1,23 @@
 <script>
 import UserProfile from '@/components/UserProfile.vue'
+
 export default {
     components: {
         UserProfile
     },
     data() {
         return {
-            userInfo: '',
-            name: '홍길동',
+            userInfo: null,
+            name: '',
             email: ''
         }
     },
     mounted() {
         this.userInfo = this.$cookies.get('user')
+        console.log(this.userInfo)
         if (this.userInfo) {
-            this.name = this.userInfo['name']
-            this.email = this.userInfo['email']
+            this.name = this.userInfo.name
+            this.email = this.userInfo.email
         }
     }
 }
@@ -25,60 +27,52 @@ export default {
         <div class="inner">
             <div class="d-flex align-items-center justify-content-between">
                 <h1 class="logo">
-                    <a href="/"><img src="/public/images/logo.svg" alt="boogle logo" /></a>
+                    <a href="/dashboard"><img src="/public/images/logo.svg" alt="boogle logo" /></a>
                 </h1>
 
                 <nav class="navbar navbar-expand-lg">
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
+                        <!-- <li class="nav-item">
                                 <router-link to="/" class="nav-link">x</router-link>
                             </li>
                             <li class="nav-item">
                                 <router-link to="/about" class="nav-link">x</router-link>
-                            </li>
-                            <li class="nav-item">
-                                <router-link to="/login" class="nav-link">로그인</router-link>
-                            </li>
+                            </li> -->
+                        <!-- 로그인한 상태에서 보여줄 탭 -->
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0" v-if="userInfo">
                             <li class="nav-item">
                                 <router-link to="/project" class="nav-link">프로젝트</router-link>
                             </li>
-
                             <li class="nav-item">
                                 <router-link to="/analyze" class="nav-link">analyze</router-link>
                             </li>
-                            <li class="nav-item">
+                            <!-- <li class="nav-item">
                                 <router-link to="/kit" class="nav-link">kit</router-link>
-                            </li>
-                            <li class="nav-item">
-                                <router-link to="/search" class="nav-link">search</router-link>
+                            </li> -->
+                            <div class="vertical-line"></div>
+                        </ul>
+
+                        <!-- 모든 상태에서 보여줄 검색 아이콘 -->
+                        <form class="d-flex mr-2">
+                            <router-link to="/search" class="nav-link">
+                                <button class="btn" type="submit"><i class="bi bi-search"></i></button>
+                            </router-link>
+                        </form>
+
+                        <div class="vertical-line"></div>
+
+                        <!-- 로그인한 상태에서 보여줄 UserProfile 드롭다운 -->
+                        <ul class="me-auto mb-2 mb-lg-0" v-if="userInfo">
+                            <li class="dropdown">
+                                <UserProfile :name="userInfo.name" :dept="userInfo.dept" />
                             </li>
                         </ul>
-                        <form class="d-flex mr-2">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i></button>
-                        </form>
-                        <ul class="me-auto mb-2 mb-lg-0" v-if="$cookies.get('user') !== null">
-                            <li class="dropdown">
-                                <UserProfile />
+
+                        <!-- 로그인하지 않은 상태에서 보여줄 로그인 탭 -->
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0" v-else>
+                            <li class="nav-item">
+                                <router-link to="/login" class="nav-link">로그인</router-link>
                             </li>
-                            <!-- <li class="dropdown">
-                                <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> 홍길동 </a>
-
-                                <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{ name }}</a>
-
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><span class="dropdown-item">개발 1팀</span></li>
-                                    <li><a class="dropdown-item" href="#">마이페이지</a></li>
-                                    <li><a class="dropdown-item" href="#">로그아웃</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider" />
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="#">Something else here</a>
-                                    </li>
-                                </ul>
-                            </li> -->
                         </ul>
                     </div>
                 </nav>
@@ -86,3 +80,17 @@ export default {
         </div>
     </header>
 </template>
+
+<style>
+.vertical-line {
+    border-left: 1px solid #eaeaea;
+    height: 40px;
+    align-self: center;
+    margin: 0 25px;
+}
+
+.navbar-nav.me-auto.mb-2.mb-lg-0 .nav-item:not(:last-child) {
+    margin-right: 20px;
+    align-self: center;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,8 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: HomeView
+            // component: HomeView
+            component: () => import('../views/LoginView.vue')
         },
         {
             path: '/about',
