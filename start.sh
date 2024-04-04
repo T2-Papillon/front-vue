cf258bec8af5fdd0085bed2e3d8ee752de1843db
choi3179 <jacobis@naer.com>
2024-04-04 16:58:10 +0900

    deploy test 35

--- start.sh
+++ start.sh
@@ -5,13 +5,14 @@ cd /home/ubuntu/app/papplan
 # 터미널 로그 확인
 set -x
 
+sudo chmod -R o+w  public/favicon.ico
+
 # vue 프로젝트 빌드
 npm install
 npm run build
 
 # 생성된 빌드 파일의 소유자 변경
 sudo chown -R ubuntu:ubuntu dist
-sudo chmod -R o+w  public
 
 # ./gradlew bootJar
 
