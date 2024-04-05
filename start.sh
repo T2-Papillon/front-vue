1af7571d9857f20f415265fc22193f288762d47f
choi3179 <jacobis@naer.com>
2024-04-05 17:22:40 +0900

    deploy test 3

--- start.sh
+++ start.sh
@@ -5,9 +5,15 @@ cd /home/ubuntu/app/papplan
 # 터미널 로그 확인
 # set -x
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                    # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # vue 프로젝트 빌드
-/home/ubuntu/.nvm/versions/node/v20.11.0/bin/npm install
-/home/ubuntu/.nvm/versions/node/v20.11.0/bin/npm run build
+# /home/ubuntu/.nvm/versions/node/v20.11.0/bin/npm install
+# /home/ubuntu/.nvm/versions/node/v20.11.0/bin/npm run build
+npm install
+npm run build
 
 # 생성된 빌드 파일의 소유자 변경
 # sudo chown -R ubuntu:ubuntu dist
