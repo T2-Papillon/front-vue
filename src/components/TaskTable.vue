4bd6c57229bc1500c5f463ba7d46b34bb4a108b9
afresh2 <afresh320@daum.net>
2024-04-11 17:19:17 +0900

    [MOD]TaskTable

--- src/components/TaskTable.vue
+++ src/components/TaskTable.vue
@@ -124,7 +124,7 @@ watch(
                     <a class="tb-project-title" @click="openModal(task)">{{ task.task_title }}</a>
                 </td>
                 <td class="text-start" v-if="showAssignee">
-                    <UserProfile :name="task.assignee_name" />
+                    <UserProfile :name="task.assignee_name" :dept="task.assignee_dept" />
                 </td>
                 <td>{{ formatDate(task.start_date) }}</td>
                 <td>{{ formatDate(task.end_date) }}</td>
