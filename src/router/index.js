c003b80078c5458dcea07c87f997fa8e70661fad
raheego <54056684+raheego@users.noreply.github.com>
2024-04-11 17:11:05 +0900

    url 인입시 login alert 없이 바로 login 화면으로

--- src/router/index.js
+++ src/router/index.js
@@ -77,7 +77,6 @@ router.beforeEach((to, from, next) => {
         if (sessionStorage.getItem('NM') != null) {
             next()
         } else {
-            alert('로그인이 필요한 페이지입니다.')
             next('/login')
         }
     }
