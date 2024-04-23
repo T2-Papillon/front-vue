69290c35d7c2fe132ddcafa8d71f81040c439b61
raheego <54056684+raheego@users.noreply.github.com>
2024-04-23 12:20:48 +0900

    이스터에그 콘솔추가

--- src/main.js
+++ src/main.js
@@ -8,6 +8,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/main.css'
 
+console.log(`
+    ____                    __
+   / __ )____  ____  ____ _/ /__
+  / __  / __ \\/ __ \\/ __ \`/ / _ \\
+ / /_/ / /_/ / /_/ / /_/ / /  __/
+/_____\/____/\\____/\\__, /_/\\___/
+                  /____/
+`)
+
 const app = createApp(App)
 
 app.use(router)
